1825b66f1b4e2e1ed7d0cadd1984c551ef8aaf5b
mehak-garg <mehakgarg741@gmail.com>
2026-03-27 15:52:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
 
-class Square extends Animal {}
+	constructor(width, height)
+{
+	this.width=width;
+	this.height=height;
+}
+get width() {
+    return this.width;
+  }
+get height() {
+    return this.width;
+  }
+	getArea(){
+		return width*height;
+	}
+}
+
+class Square extends Animal,Square {
+
+	constructor(side)
+	{super(side)
+	 
+		this.side=side;
+	}
+	getPerimeter(){
+		return 4*side;
+	}
+	width=this.side;
+	height=this.side;
+	getArea(){
+		return side*side;
+	}
+}
+
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
